6ff9d54e7a409a6a17791c0009652d5d99c13685
teddy <tkw68@nau.edu>
2022-03-27 19:46:50 -0700

    all files

--- cs205_final_exam.sh
+++ cs205_final_exam.sh
@@ -15,3 +15,15 @@
 # The data file will be passed in to the script as a positional parameter and will not
 # necessarily be called pokemon.dat. However, you can assume that any file passed to this
 # script will be formatted exactly the way pokemon.dat is formatted.
+
+echo "====== SUMMARY OF POKEMON.DAT ======"
+echo "Total Non-Legendary Pokemon:"
+#The following awk script starts at a count of zero, then adds one to the count if the value in column 12 is False.
+awk ' BEGIN {count=0;} { if ($12 == "False") count+=1} END {print count}' pokemon.dat
+echo "AVG. HP:" 
+#The following awk script counts each value in column 4, then divides it by the total number of entries in column 4.
+awk '{ total += $4; count++ } END { print total/count}' pokemon.dat
+echo "AVG. Defense:"
+#The following awk script does the same as the previous script, it counts the sum of each number in column 6, then divides the total by the number of entries.
+awk '{ sum += $6; count++ } END { print sum/count}' pokemon.dat
+echo "====== END SUMMARY ======"
